38234252f4847ae35b6bf9b08735793e53faeb12
MRiecy <3259542751@qq.com>
2025-02-11 18:46:42 +0800

    feat(components):[layout]创建了基本三栏布局

---
+++ src/components/layout/BasicLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="25%">Aside</el-aside>
+      <el-main>Main</el-main>
+      <el-aside width="25%">Aside</el-aside>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasicLayout',
+};
+</script>
+
+<style>
+.common-layout {
+  width: 100%;
+  height: 100vh; /* 使布局高度铺满整个视窗 */
+}
+
+.el-container {
+  height: 100%; /* 使容器高度铺满父容器 */
+}
+
+.el-aside {
+  background-color: #f5f5f5; /* 侧边栏背景色 */
+  padding: 20px; /* 侧边栏内边距 */
+}
+
+.el-main {
+  background-color: #ffffff; /* 主体背景色 */
+  padding: 20px; /* 主体内边距 */
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus' // 引入 Element Plus
+import 'element-plus/dist/index.css' // 引入 Element Plus 的样式
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
-
+app.use(ElementPlus) // 使用 Element Plus
 app.mount('#app')
